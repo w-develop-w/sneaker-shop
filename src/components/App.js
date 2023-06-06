fdd785ffb28599275bdaff424581d6932c23eb9c
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-06-06 13:35:05 +0300

    stage 1.4

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import './App.scss'
-import Cart from './Cart/Cart'
-import Header from './Header/Header'
+import React from "react"
+import "./App.scss"
+import Cart from "./Cart/Cart"
+import Header from "./Header/Header"
 
 function App() {
-    const [cart, setCart] = React.useState(true)
+    const [cart, setCart] = React.useState(false)
 
     return (
         <div className="App">
-            {cart ? (
-                <Header cart={cart} setCart={setCart} />
-            ) : (
-                <Cart cart={cart} setCart={setCart} />
-            )}
+            <Header cart={cart} setCart={setCart} />
+            {cart && <Cart cart={cart} setCart={setCart}/>}
         </div>
     )
 }
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,27 @@
 import styles from "./Cart.module.scss"
 
-function Cart ({cart, setCart}) {
+function Cart({ cart, setCart }) {
     return (
         <div className={styles.container}>
             <div className={styles.cart}>
                 <div>
                     <h2>Корзина</h2>
-                    <i className="ri-close-circle-line"></i>
+                    <i
+                        onClick={() => {
+                            setCart(!cart)
+                        }}
+                        className="ri-close-circle-line"
+                    ></i>
                 </div>
 
                 <h3>Итого: 5 000 грн</h3>
-                <button onClick={() => {
-					setCart(!cart)
-				}}>Оформить заказ</button>
+                <button
+                    onClick={() => {
+                        setCart(!cart)
+                    }}
+                >
+                    Оформить заказ
+                </button>
             </div>
         </div>
     )
